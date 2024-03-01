c017403bfdfae677ebf0b094aa9f72b989705baf
Alexey Y <yyexela@disroot.org>
2024-03-01 15:50:13 -0800

    Updated dataset names

--- omni_anomaly/utils.py
+++ omni_anomaly/utils.py
@@ -27,13 +27,13 @@ def save_z(z, filename='z'):
 
 
 def get_data_dim(dataset):
-    if dataset in ['SMAP', 'SMAP2']:
+    if dataset in ['SMAP', 'mypkg_SMAP']:
         return 25
-    elif dataset in ['MSL', 'MSL2']:
+    elif dataset in ['MSL', 'mypkg_MSL']:
         return 55
-    elif dataset in ['SWaT2']:
+    elif dataset in ['mypkg_SWaT']:
         return 51
-    elif str(dataset).startswith('machine')or dataset in ['SMD2']:
+    elif str(dataset).startswith('machine') or dataset in ['mypkg_SMD']:
         return 38
     else:
         raise ValueError('unknown dataset '+str(dataset))
